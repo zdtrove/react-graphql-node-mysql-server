a806e9680c7b66fee4eabe476bd8ae285cb883cf
Hoang An <antnh@icd-vn.com>
2021-03-25 16:55:51 +0700

    cm

---
+++ src/Schema/Mutations/User.ts
@@ -0,0 +1,59 @@
+import { MessageType } from './../TypeDefs/Messages';
+import { GraphQLID, GraphQLString } from "graphql";
+import { UserType } from "../TypeDefs/User";
+import { Users } from '../../Entities/Users'
+
+export const CREATE_USER = {
+    type: UserType,
+    args: {
+        name: { type: GraphQLString },
+        username: { type: GraphQLString },
+        password: { type: GraphQLString },
+    },
+    async resolve(parent: any, args: any) {
+        const { name, username, password } = args
+        await Users.insert({
+            name,
+            username,
+            password,
+        })
+        return args
+    }
+}
+
+export const UPDATE_PASSWORD = {
+    type: MessageType,
+    args: {
+        username: { type: GraphQLString },
+        oldPassword: { type: GraphQLString },
+        newPassword: { type: GraphQLString },
+    },
+    async resolve(parent: any, args: any) {
+        const { username, oldPassword, newPassword } = args
+        const user = await Users.findOne({ username })
+
+        if (!user) {
+            throw new Error("Username doesn't exist")
+        }
+        const userPassword = user?.password
+        if (oldPassword === userPassword) {
+            await Users.update({ username }, { password: newPassword })
+            return { successful: true, message: "Password updated" }
+        } else {
+            throw new Error("Password do not match!")
+        }
+    }
+}
+
+export const DELETE_USER = {
+    type: MessageType,
+    args: {
+        id: { type: GraphQLID }
+    },
+    async resolve(parent: any, args: any) {
+        const { id } = args
+        await Users.delete(id)
+
+        return { successful: true, message: "Delete worked" }
+    }
+}
